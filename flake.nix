69fe86231469efdfb8f0caea83b1751b98583ba9
Elias Vasylenko <elias@vasylenko.uk>
2025-08-26 22:03:58 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
             pname = "secret-agent";
             version = "0.1";
             src = ./.;
-            vendorHash = "sha256-IkpSjrTUxnYT5eZlx0+MJ6MBEezgpoUXcwm4GKBt3fg=";
+            vendorHash = "sha256-hXSKTS0vPY2psCG8zcivyS2hvm07LYx6dBHF73OJgYE=";
             env.CGO_ENABLED = 1;
             flags = [ "-trimpath" "-tags=linux"];
             ldflags = [
